e138cc9f1a90e92e75a515afe79de15ac42ecece
Hotpotato7420 <axejak056@gmail.com>
2023-10-18 08:57:07 +0200

    Betygssättare

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int poaeng = Integer.parseInt(JOptionPane.showInputDialog(null, "Hur många poaeng blev det?"));
+        if (poaeng > 30 || poaeng < 0) {
+            JOptionPane.showMessageDialog(null, "Felaktig data");
+        } else if (poaeng < 15) {
+            JOptionPane.showMessageDialog(null, "Provet fick ett F");
+        } else if (15 <= poaeng && poaeng < 18) {
+            JOptionPane.showMessageDialog(null, "Provet fick ett E");
+        } else if (poaeng < 21) {
+            JOptionPane.showMessageDialog(null, "Provet fick ett D");
+        } else if (poaeng < 24) {
+            JOptionPane.showMessageDialog(null, "Provet fick ett C");
+        } else if (poaeng < 27) {
+            JOptionPane.showMessageDialog(null, "Provet fick ett B");
+        } else  {
+            JOptionPane.showMessageDialog(null, "Provet fick ett A");
+        }
+    }
+}
