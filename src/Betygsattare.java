e0977ae60dcaf91c896ade2ebb42155d7499be00
WindowDorrSlide <146733215+WindowDorrSlide@users.noreply.github.com>
2023-10-18 07:15:30 +0000

    Update Betygsattare.java

--- src/Betygsattare.java
+++ src/Betygsattare.java
@@ -9,7 +9,7 @@ public class Betygsattare {
             JOptionPane.showMessageDialog(null, "Felaktig data, för högt eller lågt poäng");
         } else if (poaeng < 15) {
             JOptionPane.showMessageDialog(null, "Provet fick ett F");
-        } else if (15 <= poaeng && poaeng < 18) {
+        } else if (poaeng < 18) {
             JOptionPane.showMessageDialog(null, "Provet fick ett E");
         } else if (poaeng < 21) {
             JOptionPane.showMessageDialog(null, "Provet fick ett D");
